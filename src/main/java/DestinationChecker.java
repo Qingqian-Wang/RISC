6b5d395ec4eb3898a31502e5f4a9d584843df6ed
yuetingLuo <luoyueting522@gmail.com>
2023-04-07 00:16:24 +0800

    check if neighbor adjacent to it in checker

--- src/main/java/DestinationChecker.java
+++ src/main/java/DestinationChecker.java
@@ -13,11 +13,26 @@ public class DestinationChecker extends BasicChecker {
         int starterID = my_behavior.getOwnID();
 
         if (Objects.equals(type, "Attack")) {
-            int behavior_DestinationID = my_behavior.getDestination().getOwnID();
-            if (starterID == behavior_DestinationID){
-                return "you are attacking, input a place not belong to you!";
+            int enemyID = my_behavior.getDestination().getOwnID();
+            if (starterID == enemyID){
+                return "you are attacking you own place!";
             }
-            //neighbor
+            //check if neighbor adjacent to it
+            boolean adjacent = false;
+            String enemyName = my_behavior.getDestination().getName();
+            if(my_behavior.getOrigin().getNeighbor().containsKey(enemyID)) {
+                for(int i = 0; i < my_behavior.getOrigin().getNeighbor().get(enemyID).size(); i++){
+                    if(enemyName.equals(my_behavior.getOrigin().getNeighbor().get(enemyID).get(i))){
+                        adjacent = true;
+                        break;
+                    }
+                }
+            }
+
+            if(!adjacent){
+                return "Enemy not adjacent to you";
+            }
+
         } else if (Objects.equals(type, "Move")){
             int behavior_DestinationID = my_behavior.getDestination().getOwnID();
             if (starterID != behavior_DestinationID){
