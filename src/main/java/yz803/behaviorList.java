440e62b7b30858f7d6ff1bd20de1b2909f718228
yuetingLuo <luoyueting522@gmail.com>
2023-04-06 23:34:49 +0800

    send and receive in globalMap and behaviorList

--- src/main/java/yz803/behaviorList.java
+++ src/main/java/yz803/behaviorList.java
@@ -1,30 +1,30 @@
 package yz803;
 
-import java.security.PublicKey;
-import java.util.ArrayList;
+
+import java.net.Socket;
 
 public class behaviorList {
-	private ArrayList<Behavior> moveList;
-	private ArrayList<Behavior> attackList;
-	private int playerID;
-
-	public ArrayList<Behavior> getMoveList() {
-		return moveList;
-	}
-
-	public ArrayList<Behavior> getAttackList() {
-		return attackList;
-	}
-
-	public int getPlayerID() {
-		return playerID;
-	}
-
-	public void sendList(socket) {
-		
-	}
-	
-	public Void receiveList(socket) {
-		
-	}
+//	private ArrayList<Behavior> moveList;
+//	private ArrayList<Behavior> attackList;
+//	private int playerID;
+//
+//	public ArrayList<Behavior> getMoveList() {
+//		return moveList;
+//	}
+//
+//	public ArrayList<Behavior> getAttackList() {
+//		return attackList;
+//	}
+//
+//	public int getPlayerID() {
+//		return playerID;
+//	}
+//
+//	public void sendList(Socket socket) {
+//
+//	}
+//
+//	public void receiveList(Socket socket) {
+//
+//	}
 }
