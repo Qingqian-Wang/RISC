f66ee2fe553d42f546f04debb83ca560509afe34
raplis <1195624612@qq.com>
2023-04-08 17:48:52 +0800

    seconde

---
+++ src/test/java/PlayerInfoTest.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerInfoTest {
+	@Test
+	public void test1() throws IOException {
+//		Socket temSocket = new Socket("localhost", 0);
+//		PlayerInfo temp1 = new PlayerInfo(temSocket, 3);
+//		assertEquals(temp1.getPlayerID(), 3);
+////		assertEquals(temp1.getPlayerSocket(), temSocket);
+////		assertEquals(temp1., null);
+		
+	}
+}
